2f9f319ea43bf4b521cc462523dbd8cf59e56fba
motirase <117908555+motirase@users.noreply.github.com>
2023-07-21 15:09:44 +0300

    Update 2-c_route.py

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,53 +1,28 @@
 #!/usr/bin/python3
 """
-<<<<<<< HEAD
-Script that starts a Flask web application
-"""
-from flask import Flask
-
-
-=======
 starts a Flask web application
 """
 
 from flask import Flask
->>>>>>> 8c5dbeed60929e24fc56e598218afe8c5214768b
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-<<<<<<< HEAD
-def hello():
-=======
 def index():
     """returns Hello HBNB!"""
->>>>>>> 8c5dbeed60929e24fc56e598218afe8c5214768b
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
-<<<<<<< HEAD
-def holberon():
-=======
 def hbnb():
     """returns HBNB"""
->>>>>>> 8c5dbeed60929e24fc56e598218afe8c5214768b
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-<<<<<<< HEAD
-def c_text(text):
-    return "C {}" .format(text.replace("_", " "))
-
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
-=======
 def cisfun(text):
     """display “C ” followed by the value of the text variable"""
     return 'C ' + text.replace('_', ' ')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
->>>>>>> 8c5dbeed60929e24fc56e598218afe8c5214768b
