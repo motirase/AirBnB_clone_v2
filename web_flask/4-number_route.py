3df7499c316850337a666c178570775a4fd82d82
motirase <117908555+motirase@users.noreply.github.com>
2023-07-21 15:13:07 +0300

    Update 4-number_route.py

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,61 +1,25 @@
 #!/usr/bin/python3
 """
-<<<<<<< HEAD
-Script that starts a Flask web application
-"""
-from flask import Flask
-
-
-=======
 starts a Flask web application
 """
 
 from flask import Flask
->>>>>>> 8c5dbeed60929e24fc56e598218afe8c5214768b
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-<<<<<<< HEAD
-def hello():
-=======
 def index():
     """returns Hello HBNB!"""
->>>>>>> 8c5dbeed60929e24fc56e598218afe8c5214768b
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
-<<<<<<< HEAD
-def holberon():
-=======
 def hbnb():
     """returns HBNB"""
->>>>>>> 8c5dbeed60929e24fc56e598218afe8c5214768b
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-<<<<<<< HEAD
-def c_text(text):
-    return "C {}" .format(text.replace("_", " "))
-
-
-@app.route('/python/', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def python_text(text='is cool'):
-    """ Function called with /python/<text> route """
-    return "Python {}".format(text.replace("_", " "))
-
-
-@app.route('/number/<int:n>', strict_slashes=False)
-def is_a_number(n):
-    return "{} is a number".format(n)
-
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
-=======
 def cisfun(text):
     """display “C ” followed by the value of the text variable"""
     return 'C ' + text.replace('_', ' ')
@@ -75,4 +39,3 @@ def imanumber(n):
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
->>>>>>> 8c5dbeed60929e24fc56e598218afe8c5214768b
