95a574d125756ade14774ed287984e4d172800f9
motirase <117908555+motirase@users.noreply.github.com>
2023-07-22 17:53:30 +0300

    Update 8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,30 +1,28 @@
 #!/usr/bin/python3
+"""
+flask model
+"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-from models.city import City
-
-
 app = Flask(__name__)
+storage.all()
 
 
 @app.teardown_appcontext
-def tear_down(self):
-    """tear down app context"""
+def teardown_data(self):
+    """
+        refrech data
+    """
     storage.close()
 
 
 @app.route('/cities_by_states', strict_slashes=False)
-def list_states():
-    """lists states from database
-    Returns:
-        HTML
-    """
-    dict_states = storage.all(State)
-    all_states = []
-    for k, v in dict_states.items():
-        all_states.append(v)
-    return render_template('8-cities_by_states.html', all_states=all_states)
+def cities_by_states():
+    """ return all citie in the db  """
+    states = storage.all(State)
+
+    return render_template('8-cities_by_states.html', states=states)
 
 
 if __name__ == "__main__":
