9e243faba4522cb84f44b9076be100c5ad7f8630
motirase <117908555+motirase@users.noreply.github.com>
2023-08-17 14:26:17 +0300

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 @app.route('/airbnb-onepage/', strict_slashes=False)
 def hello():
     """returns Hello HBNB!"""
-    return render_template("10-hbnb_filters.html")
+    return "10-hbnb_filters.html"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
